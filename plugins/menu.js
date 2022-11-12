c0a943f238ab335f82393c2b136fd093a6425bd4
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-12 12:15:28 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -190,7 +190,7 @@ let handler = async (m, { conn, usedPrefix, __dirname }) => {
 contextInfo: { externalAdReply :{ showAdAttribution: true,
                         sourceUrl: 'https://www.youtube.com/watch?v=bBjIEByO5ts',
                         mediaType: 2,
-                        description: `⚘ ᴄʀᴇᴀᴛᴇ ᴹᴿ᭄ King Of Bear ×፝֟͜×`,
+                        description: `𝕊𝕖𝕔𝕣𝕖𝕥 𝕄𝔻`,
                         title: `💌 Ultah mu : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
                         body: `⚘ ᴄʀᴇᴀᴛᴇ by 𝓡𝓮𝓮𝔂𝓪`,          previewType: 0,
                         thumbnail: await (await fetch(giflogo2)).buffer(),
